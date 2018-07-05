a75201754fbb600179df23c74bb8076e06fe1ef2
ddaaat <sadhat91@gmail.com>
2018-07-05 12:00:22 +0800

    update2

--- activity5a/index.php
+++ activity5a/index.php
@@ -8,12 +8,13 @@ $fruit5 = array("Orange","orangu","50");
 $arr = array($fruit1,$fruit2,$fruit3,$fruit4,$fruit5);
 
 
-
+echo "<table>";
 for($a = 0 ; $a<5 ; $a++)
 {	
-	echo $arr[$a][0]." " . $arr[$a][1] ." " . $arr[$a][2]; 
-	
+	echo "<tr>";
+	echo "<td>" . $arr[$a][0] . "</td> <td>" . $arr[$a][1] . "</td><td> " .$arr[$a][2]. "</td>"; 
 	echo "<br>";
 }
+echo "</table>";
 
 ?>
