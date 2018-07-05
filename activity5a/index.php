354b9b7fa867795ddb00cba2ad68961b5cb2c921
ddaaat <sadhat91@gmail.com>
2018-07-05 11:22:30 +0800

    Fruit List

---
+++ activity5a/index.php
@@ -0,0 +1,16 @@
+<?php
+$fruit1 = array("Apple","apol","10");
+$fruit2 = array("Banana","banban","30");
+$fruit3 = array("Citrus","citri","20");
+$fruit4 = array("Grapes","grapa","40");
+$fruit5 = array("Orange","orangu","50");
+
+$arr = ($fruit1,$fruit2,$fruit3,$fruit4,$fruit5);
+
+for($a = 0 ; $a <5 ; $a++)
+{
+	echo $arr; 
+	echo "<br>";
+}
+
+?>
