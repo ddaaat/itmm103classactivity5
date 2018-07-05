ed799fe2da5badcc0f584a8b79648cb8ec3d8ea7
ddaaat <sadhat91@gmail.com>
2018-07-05 11:56:03 +0800

    Update1

--- activity5a/index.php
+++ activity5a/index.php
@@ -5,11 +5,14 @@ $fruit3 = array("Citrus","citri","20");
 $fruit4 = array("Grapes","grapa","40");
 $fruit5 = array("Orange","orangu","50");
 
-$arr = ($fruit1,$fruit2,$fruit3,$fruit4,$fruit5);
+$arr = array($fruit1,$fruit2,$fruit3,$fruit4,$fruit5);
 
-for($a = 0 ; $a <5 ; $a++)
-{
-	echo $arr; 
+
+
+for($a = 0 ; $a<5 ; $a++)
+{	
+	echo $arr[$a][0]." " . $arr[$a][1] ." " . $arr[$a][2]; 
+	
 	echo "<br>";
 }
 
